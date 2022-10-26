accc03c4a3c29398e59f91a7669a02177723d53b
Mickael Sermont <sermont.mickael@hotmail.fr>
2022-10-26 11:05:02 +0200

    feat: add react-icons & icons in home

--- src/components/Cards/HomeCard/index.jsx
+++ src/components/Cards/HomeCard/index.jsx
@@ -3,29 +3,13 @@ import { Link } from "react-router-dom";
 
 function HomeCard({ data }) {
   return (
-    <Link to={data.href}>
+    <Link to={data.href} className="group">
       <article className="rounded-lg border border-gray-100 p-4 shadow-sm transition hover:shadow-lg sm:p-6">
         <span className="inline-block rounded bg-green-600 p-2 text-white">
-          {/* {data.icon} */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path d="M12 14l9-5-9-5-9 5 9 5z" />
-            <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-            />
-          </svg>
+          {data.icon}
         </span>
 
-        <h3 className="mt-0.5 text-lg font-medium text-gray-900">
+        <h3 className="mt-2.5 text-lg font-medium text-gray-900">
           {data.title}
         </h3>
 
@@ -33,9 +17,8 @@ function HomeCard({ data }) {
           {data.description}
         </p>
 
-        <a
-          href="#"
-          className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-green-600"
+        <div
+          className="mt-4 inline-flex items-center gap-1 text-sm font-medium text-green-600"
         >
           Find out more
           <span
@@ -44,7 +27,7 @@ function HomeCard({ data }) {
           >
             &rarr;
           </span>
-        </a>
+        </div>
       </article>
     </Link>
   );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import HomeCard from "components/Cards/HomeCard";
+import { HiUserGroup, HiUserPlus } from 'react-icons/hi2';
 
 const homeInfos = [
   {
     id: 1,
     href: "/employees/new",
+    icon: <HiUserPlus className="w-7 h-7" />,
     title: "Create new employee",
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus pariatur animi temporibus nesciunt praesentium dolore sed",
   },
   {
     id: 2,
     href: "/employees",
+    icon: <HiUserGroup className="w-7 h-7" />,
     title: "List employees",
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus pariatur animi temporibus nesciunt praesentium dolore sed",
   },
