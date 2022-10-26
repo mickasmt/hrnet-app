91e5090ae63c5dc10bffbbb0273bafe397b0ff2c
Mickael Sermont <sermont.mickael@hotmail.fr>
2022-10-26 10:13:29 +0200

    feat: add tailwindcss & home styles

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 import HomeCard from "components/Cards/HomeCard";
 
+const homeInfos = [
+  {
+    id: 1,
+    href: "/employees/new",
+    title: "Create new employee",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus pariatur animi temporibus nesciunt praesentium dolore sed",
+  },
+  {
+    id: 2,
+    href: "/employees",
+    title: "List employees",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus pariatur animi temporibus nesciunt praesentium dolore sed",
+  },
+];
+
 function Home() {
   return (
-    <main className="homeContainer">
-      <div className="homeCards">
-        <HomeCard
-          href="/employees/new"
-          title="Create employee"
-          description="Little description"
-          icon={<span>icon1</span>}
-        />
-
-        <HomeCard
-          href="/employees/new"
-          title="List employees"
-          description="Little description"
-          icon={<span>icon2</span>}
-        />
+    <main className="h-3/4 w-full flex justify-center items-center f-container">
+      <div className="flex space-x-5">
+        {homeInfos.map((info) => {
+          return <HomeCard data={info} key={info.id} />;
+        })}
       </div>
     </main>
   );
