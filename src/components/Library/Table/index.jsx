ed57b3536746fc68453a26a5d980af17e0fc8f58
Mickael Sermont <sermont.mickael@hotmail.fr>
2022-11-02 22:22:56 +0100

    feat: add date-fns & table for employees

---
+++ src/components/Library/Table/index.jsx
@@ -0,0 +1,213 @@
+import React from "react";
+
+function Table({ data, columns }) {
+  return (
+    <div>
+      <div class="flex justify-between items-center pb-4">
+        <div>
+          <button
+            id="dropdownRadioButton"
+            data-dropdown-toggle="dropdownRadio"
+            class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5"
+            type="button"
+          >
+            <svg
+              class="mr-2 w-4 h-4 text-gray-400"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            Last 30 days
+            <svg
+              class="ml-2 w-3 h-3"
+              aria-hidden="true"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M19 9l-7 7-7-7"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <label for="table-search" class="sr-only">
+          Search
+        </label>
+        <div class="relative">
+          <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <svg
+              class="w-5 h-5 text-gray-500"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <input
+            type="text"
+            id="table-search"
+            class="block p-2 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+            placeholder="Search for items"
+          />
+        </div>
+      </div>
+
+      <div class="flex flex-col">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="overflow-x-auto">
+              <table class="min-w-full border text-center">
+                <thead class="border-b">
+                  <tr>
+                    {columns.map((column) => {
+                      return (
+                        <th
+                          scope="col"
+                          class="text-sm font-medium text-gray-900 px-5 py-4 border-r"
+                        >
+                          {column.title}
+                        </th>
+                      );
+                    })}
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.map((obj) => {
+                    return (
+                      <tr class="border-b">
+                        {Object.values(obj).map((value) => {
+                          return (
+                            <td class="text-sm text-gray-900 font-light px-5 py-4 whitespace-nowrap border-r">
+                              {value}
+                            </td>
+                          );
+                        })}
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <nav
+        class="flex justify-between items-center pt-4"
+        aria-label="Table navigation"
+      >
+        <span class="text-sm font-normal text-gray-500 dark:text-gray-400">
+          Showing{" "}
+          <span class="font-semibold text-gray-900 dark:text-white">1-10</span>{" "}
+          of{" "}
+          <span class="font-semibold text-gray-900 dark:text-white">1000</span>
+        </span>
+        <ul class="inline-flex items-center -space-x-px">
+          <li>
+            <a
+              href="#"
+              class="block py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              <span class="sr-only">Previous</span>
+              <svg
+                class="w-5 h-5"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              1
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              2
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              aria-current="page"
+              class="z-10 py-2 px-3 leading-tight text-blue-600 bg-blue-50 border border-blue-300 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+            >
+              3
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              ...
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              100
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="block py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              <span class="sr-only">Next</span>
+              <svg
+                class="w-5 h-5"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Table;
--- src/pages/ListEmployees/index.jsx
+++ src/pages/ListEmployees/index.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import Title from "components/UI/Title";
+import Table from "components/Library/Table";
+
+// data
+import data from "data/employees.json";
+
+const columns = [
+  { title: 'First Name', data: 'firstName' },
+  { title: 'Last Name', data: 'lastName' },
+  { title: 'Start Date', data: 'startDate' },
+  { title: 'Department', data: 'department' },
+  { title: 'Date of Birth', data: 'dateOfBirth' },
+  { title: 'Street', data: 'street' },
+  { title: 'City', data: 'city' },
+  { title: 'State', data: 'state' },
+  { title: 'Zip Code', data: 'zipCode' },
+];
 
 function ListEmployees() {
   return (
     <main className="f-container pt-6 pb-10 md:py-12">
       <Title text="List Employees" />
 
-
+      <Table data={data.employees} columns={columns} />
     </main>
   );
 }
